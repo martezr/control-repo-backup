65e5f358fb0c3b41a0665513272ff97a9304dc99
Martez Reed <martez.reed@gmail.com>
2021-05-19 14:46:19 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,8 @@ File { backup => false }
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  if $trusted['extensions']['pp_role'] {
+    $role = $trusted['extensions']['pp_role']
+    include "role::${::role}"
+  }
 }
