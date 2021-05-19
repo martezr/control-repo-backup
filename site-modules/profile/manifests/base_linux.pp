e86950839495ebaf988c317dece3ea5250291fd6
Martez Reed <martez.reed@gmail.com>
2021-05-19 14:59:15 -0500

    Update base_linux.pp

--- site-modules/profile/manifests/base_linux.pp
+++ site-modules/profile/manifests/base_linux.pp
@@ -1,7 +1,7 @@
 class profile::base_linux {
    class {'::secure_linux_cis':
-      time_servers  => ['tick.usno.navy.mil', 'tock.usno.navy.mil'],
+      time_servers  => ['0.pool.ntp.org', '1.pool.ntp.org'],
       profile_type  => 'server',
-      allow_users   => 'trusteduser',
+      allow_users   => ['trusteduser'],
    }
 }
