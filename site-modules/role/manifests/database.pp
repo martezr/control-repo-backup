c9118ba0cf4fa73e51383be331185a1595a858c4
Martez Reed <martez.reed@gmail.com>
2021-05-20 10:27:10 -0500

    Update database.pp

--- site-modules/role/manifests/database.pp
+++ site-modules/role/manifests/database.pp
@@ -2,7 +2,6 @@ class role::database {
   include profile::base_linux
   class { 'postgresql::server':
     ip_mask_allow_all_users => '0.0.0.0/0',
-    pg_hba_conf_defaults    => false,
     listen_addresses        => '0.0.0.0',
     postgres_password       => 'TPSrep0rt!',
   }
