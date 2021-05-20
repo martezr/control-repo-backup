c45339a491446a4ff21bea211f9165b62185be87
Martez Reed <martez.reed@gmail.com>
2021-05-20 07:21:19 -0500

    Update database.pp

--- site-modules/role/manifests/database.pp
+++ site-modules/role/manifests/database.pp
@@ -3,4 +3,12 @@ class role::database {
   class { 'postgresql::server':
     postgres_password => 'TPSrep0rt!',
   }
+   firewall { '015 postgresql port':
+    chain  => 'INPUT',
+    dport  => 5432,
+    state  => 'NEW',
+    action => 'accept',
+    proto  => 'tcp',
+    tag    => 'cis_firewall_rule',
+  }
 }
